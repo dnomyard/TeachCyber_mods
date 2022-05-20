5bf0a262cb991a08108a3e35d3ab7c61bb0f68b6
D Raymond <dnomyard@gmail.com>
2022-05-20 12:37:44 -0400

    modified config script for TeachCyber Kali instance

--- config_scripts/Kali_desktop.sh
+++ config_scripts/Kali_desktop.sh
@@ -2,18 +2,19 @@
 
 
 
-wget https://github.com/dnomyard/ECE5586_environment/raw/main/artifacts/lab_files/lab1/buffer_oflow.tar -P /home/student/lab1/
-wget https://raw.githubusercontent.com/dnomyard/ECE5586_environment/main/artifacts/lab_files/lab1/user_hashes.txt -P /home/student/lab1/
-sudo wget https://raw.githubusercontent.com/dnomyard/ECE5586_environment/main/artifacts/lab_files/lab2/default_firewall.sh -P /etc/
-sudo wget https://raw.githubusercontent.com/dnomyard/ECE5586_environment/main/artifacts/lab_files/lab2/extingui.sh -P /etc/
-wget https://raw.githubusercontent.com/dnomyard/ECE5586_environment/main/artifacts/lab_files/lab2/firewall.sh -P /home/student/lab2/
+wget https://github.com/dnomyard/TeachCyber_mods/raw/main/artifacts/lab_files/lab1/buffer_oflow.tar -P /home/student/lab1/
+wget https://raw.githubusercontent.com/dnomyard/TeachCyber_mods/main/artifacts/lab_files/lab2/default_firewall.sh -P /etc/
+sudo mv default_firewall.sh /etc/
+wget https://raw.githubusercontent.com/dnomyard/TeachCyber_mods/main/artifacts/lab_files/lab2/extingui.sh -P /etc/
+sudo mv extingui.sh /etc/
+wget https://raw.githubusercontent.com/dnomyard/TeachCyber_mods/main/artifacts/lab_files/lab2/firewall.sh -P /home/student/lab2/
 sudo chmod +x /etc/default_firewall.sh
 sudo chmod +x /etc/extingui.sh
 chmod +x /home/student/lab2/firewall.sh
 sudo cp /home/student/lab2/firewall.sh /etc/
 #########################
 ## minor cleanup        #
-cd ~
-sudo rm *.sh
-history -c
+#cd ~
+#sudo rm *.sh
+#history -c
 
