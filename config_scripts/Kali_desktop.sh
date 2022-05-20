821f0433725528994a3ea25f7038c74f4ba5e649
D Raymond <dnomyard@gmail.com>
2022-05-20 12:40:06 -0400

    corrected subfolders names for repo files

--- config_scripts/Kali_desktop.sh
+++ config_scripts/Kali_desktop.sh
@@ -1,13 +1,10 @@
 # Create folders and place artifacts
-
-
-
-wget https://github.com/dnomyard/TeachCyber_mods/raw/main/artifacts/lab_files/lab1/buffer_oflow.tar -P /home/student/lab1/
-wget https://raw.githubusercontent.com/dnomyard/TeachCyber_mods/main/artifacts/lab_files/lab2/default_firewall.sh -P /etc/
+wget https://github.com/dnomyard/TeachCyber_mods/raw/main/artifacts/lab1/buffer_oflow.tar -P /home/student/lab1/
+wget https://raw.githubusercontent.com/dnomyard/TeachCyber_mods/main/artifacts/lab2/default_firewall.sh -P /etc/
 sudo mv default_firewall.sh /etc/
-wget https://raw.githubusercontent.com/dnomyard/TeachCyber_mods/main/artifacts/lab_files/lab2/extingui.sh -P /etc/
+wget https://raw.githubusercontent.com/dnomyard/TeachCyber_mods/main/artifacts/lab2/extingui.sh -P /etc/
 sudo mv extingui.sh /etc/
-wget https://raw.githubusercontent.com/dnomyard/TeachCyber_mods/main/artifacts/lab_files/lab2/firewall.sh -P /home/student/lab2/
+wget https://raw.githubusercontent.com/dnomyard/TeachCyber_mods/main/artifacts/lab2/firewall.sh -P /home/student/lab2/
 sudo chmod +x /etc/default_firewall.sh
 sudo chmod +x /etc/extingui.sh
 chmod +x /home/student/lab2/firewall.sh
